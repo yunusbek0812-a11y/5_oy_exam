6cd58273e70157c4346240d3352d837808371746
yunusbek0812-a11y <yunusbek0812@gmail.com>
2026-06-15 16:03:19 +0500

    exam_5

--- index.js
+++ index.js
@@ -4,41 +4,51 @@ const cors = require("cors");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+
 const connectDB = require("./config/db.config");
+
 const authRouter = require("./router/auth.router");
 const categoryRouter = require("./router/category.router");
 const carRouter = require("./router/car.router");
+
 const errorMiddleware = require("./middleware/error.middleware");
 const logger = require("./utils/logger");
 
 const app = express();
-const PORT = process.env.PORT || 4000;
 
-// Middleware
+/* CAR PORT */
+const PORT = process.env.PORT || 4001;
+
+/* Swagger */
+const swaggerDocument = YAML.load(path.join(__dirname, "./docs/swagger.yml"));
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+/* Middleware */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: process.env.CLIENT_URL || "*",
-  credentials: true,
-}));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(cookieParser());
 
-// Static files
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// Connect DB
+/* DB */
 connectDB();
 
-// Routes
+/* Routes */
 app.use("/api", authRouter);
 app.use("/api", categoryRouter);
 app.use("/api", carRouter);
 
 
-
-// Error handler
 app.use(errorMiddleware);
 
+
+/* Server */
 app.listen(PORT, () => {
-  logger.info(`Server ishga tushdi:`+ PORT);
+  logger.info(` Car API: http://localhost:${PORT}`);
+  logger.info(` Swagger: http://localhost:${PORT}/api-docs`);
 });
